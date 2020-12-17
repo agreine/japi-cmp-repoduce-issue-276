c27d35f31ce3514cb781e44300af7fd2e7be864c
Greiner, Andreas <Andreas.greiner@de-gmbh.com>
2020-12-17 15:54:01 +0100

    classes changed

--- src/main/java/com/desoft/japicmp/issue276/B.java
+++ src/main/java/com/desoft/japicmp/issue276/B.java
@@ -1,6 +1,16 @@
 package com.desoft.japicmp.issue276;
 
-public interface B extends A
+/**
+ * TODO: Add class description!
+ *
+ * @version $Id: my.products.setup,v 1.44 2017/03/09 09:44:26 rh Exp $<br>
+ * (c) DE software &amp; control GmbH
+ */
+public class B
 {
-    // empty
+    @Override
+    public String toString()
+    {
+        return "B";
+    }
 }
--- src/main/java/com/desoft/japicmp/issue276/BImpl.java
+++
@@ -1,7 +0,0 @@
-package com.desoft.japicmp.issue276;
-
-/** Implements the method {@link #foo()} */
-public class BImpl extends BAbstract
-{
-
-}
